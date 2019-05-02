766c3e82264f86c8bffa5fe8ddf956f449d82026
Jonas Gunz <himself@jonasgunz.de>
2019-05-02 23:23:44 +0200

    Added polybar

--- install.sh
+++ install.sh
@@ -6,7 +6,10 @@ git submodule update
 mv ~/.config/i3/ ~/.config/i3.old
 mv ~/.vim ~/.vim.old
 mv ~/.xinitrc ~/.xinitrc.old
+mv ~/.config/polybar ~/.config/polybar.old
 
 ln -s $(pwd)/.vim ~/.vim
 ln -s $(pwd)/.i3 ~/.i3
 ln -s $(pwd)/.xinitrc ~/.xinitrc
+ln -s $(pwd)/polybar ~/.config/polybar
+
