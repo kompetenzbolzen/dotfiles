1ba3574cefc5b9b56ea8456a9645a5723586334b
Jonas Gunz <himself@jonasgunz.de>
2020-06-06 15:37:16 +0200

    modified prompt

--- bash/prompt.bash
+++ bash/prompt.bash
@@ -2,11 +2,13 @@
 
 function prompt_command () {
 	local EXIT="$?"
-	if [ $EXIT -eq 0 ]; then
-		EXIT=""
-	fi
-	#PS1="\n${cyan}\h:$(virtualenv_prompt) ${reset_color} ${yellow}\w ${green}$(scm_prompt_info)\n${red}${EXIT} ${reset_color}→ "
-	PS1="\n${cyan}\h: ${reset_color} ${yellow}\w \n${red}${EXIT} ${reset_color}→ "
+	local REMOTE=""
+
+	[ $EXIT -eq 0 ] && EXIT=""
+	[ ! -z "$SSH_CONNECTION" ] && REMOTE="${orange}[R] "
+	
+	#PS1="\n${REMOTE}${cyan}\h:$(virtualenv_prompt) ${reset_color} ${yellow}\w ${green}$(scm_prompt_info)\n${red}${EXIT} ${reset_color}→ "
+	PS1="\n${REMOTE}${cyan}\h: ${reset_color} ${yellow}\w \n${red}${EXIT} ${reset_color}→ "
 }
 
 PROMPT_COMMAND=prompt_command
