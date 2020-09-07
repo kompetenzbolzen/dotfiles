97a85d875127da7e1760c7dfb03e2a5f773448a9
Jonas Gunz <himself@jonasgunz.de>
2020-09-07 23:00:05 +0200

    gnome keyring

--- xinitrc.d/00-misc.sh
+++ xinitrc.d/00-misc.sh
@@ -6,3 +6,7 @@ setxkbmap -layout de
 /usr/bin/setxkbmap -option "caps:swapescape"
 
 redshift -l 48.814:11.3431 &
+
+/usr/bin/gnome-keyring-daemon --start --components=pkcs11,secrets &
+
+nextcloud &
---
+++ xinitrc.d/10-mqtt-lock.sh
@@ -0,0 +1,3 @@
+#!/bin/sh
+
+mqtt_monitor_systemlock.sh mqtt.muc.jag.re monitor/hata/jonas &
