a63880722047e6bca33a6babf2291547280fb1c0
Jonas Gunz <himself@jonasgunz.de>
2020-11-16 14:15:39 +0100

    prompt venv

--- bash/prompt.bash
+++ bash/prompt.bash
@@ -3,12 +3,14 @@
 function prompt_command () {
 	local EXIT="$?"
 	local REMOTE=""
+	local VENV=""
 
 	[ $EXIT -eq 0 ] && EXIT=""
 	[ ! -z "$SSH_CONNECTION" ] && REMOTE="${orange}[R] "
+	[ -z "$VIRTUAL_ENV" ] || VENV="$(basename "$VIRTUAL_ENV")"
 	
 	#PS1="\n${REMOTE}${cyan}\h:$(virtualenv_prompt) ${reset_color} ${yellow}\w ${green}$(scm_prompt_info)\n${red}${EXIT} ${reset_color}→ "
-	PS1="\n${REMOTE}${cyan}\h: ${reset_color} ${yellow}\w \n${red}${EXIT} ${reset_color}→ "
+	PS1="\n${REMOTE}${cyan}\h: ${reset_color} ${yellow}\w ${green}${VENV}\n${red}${EXIT} ${reset_color}→ "
 }
 
 PROMPT_COMMAND=prompt_command
