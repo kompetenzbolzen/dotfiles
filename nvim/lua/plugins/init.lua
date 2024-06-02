1d1ecaf9392ee3a2e113ac24786a26718218c8e4
Jonas Gunz <himself@jonasgunz.de>
2024-06-02 14:50:54 +0200

    add treesitter

--- nvim/lua/plugins/init.lua
+++ nvim/lua/plugins/init.lua
@@ -7,5 +7,16 @@ return {
 	},
 	{
 		"jamessan/vim-gnupg"
-	}
+	},
+	{
+                "nvim-treesitter/nvim-treesitter",
+                config = function()
+                        require'nvim-treesitter.configs'.setup{
+                                ensure_installed = {"c", "cpp", "lua", "vim", "bash", "make", "rust"},
+                                highlight = {
+                                        enable = true
+                                }
+                        }
+                end
+        }
 }
