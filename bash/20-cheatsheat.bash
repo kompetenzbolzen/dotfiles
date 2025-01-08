9de9d34eaa760705f5ec643d1798cc25ec5f0663
Jonas Gunz <himself@jonasgunz.de>
2025-01-08 03:45:02 +0100

    cheatsheet

---
+++ bash/20-cheatsheat.bash
@@ -0,0 +1,44 @@
+CHEATSHEAT_COLUMNS=3
+CHEATSHEAT_PART_LEFT=0.4
+CHEATSHEAT_CONF_FILE="$DOTFILEBASE/cheatsheet"
+
+CHEATSHEAT_COLOR_PRIMARY=$purple
+CHEATSHEAT_COLOR_SECONDARY=$white
+
+#CHEATSHEAT_ENABLE defaults to no
+
+function __cheatsheat_print() {
+	echo -n "$(printf "$@")"
+}
+
+function __cheatsheet() {
+	local COLS COL_L COL_R SINGLE_COLUMN CNT CP CS
+	COLS=$(tput cols)
+	SINGLE_COLUMN=$(bc <<< "$COLS/$CHEATSHEAT_COLUMNS")
+	COL_L=$(bc <<< "$SINGLE_COLUMN * $CHEATSHEAT_PART_LEFT")
+	COL_R=$(bc <<< "$SINGLE_COLUMN * (1.0-$CHEATSHEAT_PART_LEFT)")
+	COL_L=${COL_L%.*}
+	COL_R=${COL_R%.*}
+
+	CP=$CHEATSHEAT_COLOR_PRIMARY
+	CS=$CHEATSHEAT_COLOR_SECONDARY
+
+	CNT=0
+	# TODO CSV nur einmal einlesen
+	while IFS=";" read -r A B; do
+		__cheatsheat_print "$CP%-${COL_L}s$reset_color$CS%-${COL_R}s$reset_color" \
+			"$A" "$B"
+		CNT=$(((CNT+1)%3))
+		test $CNT -eq 0 && echo
+	done < $CHEATSHEAT_CONF_FILE
+}
+
+function __clear() {
+	\clear
+	__cheatsheet
+}
+
+if [ "$CHEATSHEAT_ENABLE" = "yes" ]; then
+	alias clear="__clear"
+	__cheatsheet
+fi
