8a3035a36fdfdd547c31007bc70fec68226c7358
Jonas Gunz <himself@jonasgunz.de>
2025-11-05 00:01:54 +0100

    startmail on c

---
+++ scripts/startmail.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+
+SESSIONNAME='neomutt'
+INTERVAL=30
+TERMINAL=alacritty
+
+function check_session() {
+	tmux list-sessions -F '#S' | grep '^neomutt$' > /dev/null 2>&1
+}
+
+function attach_session() {
+	tmux attach -t "$SESSIONNAME:neomutt"
+}
+
+function new_session() {
+	tmux new-session -s "$SESSIONNAME" -n 'neomutt' -d "bash -c 'while true; do neomutt; done'"
+	tmux new-window -n "mbsync" -t ${SESSIONNAME}: "bash -c 'while true; do mbsync -a; sleep $INTERVAL; done'"
+	
+	attach_session
+}
+
+check_session || new_session
+attach_session
