4aeb0d7d16c894dec22ef049b13a123825ba0a3a
Jonas Gunz <himself@jonasgunz.de>
2026-02-04 15:40:03 +0100

    tabs

---
+++ nvim/lua/plugins/tabs.lua
@@ -0,0 +1,25 @@
+return {
+	{
+		"FotiadisM/tabset.nvim",
+		opts = {
+			defaults ={
+				tabwidth = 8,
+				expandtab = false
+			},
+			languages ={
+				haskell = {
+					tabwidth = 4,
+					expandtab = false
+				},
+				python = {
+					tabwidth = 4,
+					expandtab = false
+				},
+				openscad = {
+					tabwidth = 2,
+					expandtab = true
+				},
+			},
+		}
+	}
+}
