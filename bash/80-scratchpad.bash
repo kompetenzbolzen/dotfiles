33aee03949a04941d5fe538ff4d8cda1dd199973
Jonas Gunz <himself@jonasgunz.de>
2026-09-10 03:42:08 +0200

    scratchpad enter command

--- bash/80-scratchpad.bash
+++ bash/80-scratchpad.bash
@@ -26,14 +26,17 @@ function __sp_genereate_mount_path() {
 
 # 1: output 2: template dir
 function __sp_new_pad() {
-	tar -cf "$1" -C "$2" .
+	(
+	cd "$2" || return
+	tar -cf "$1" -- *
+	)
 }
 
 # 1: archive 2: pad file
 function __sp_mount_pad() {
 	mkdir -p "$pad_mnt" || return
 	archivemount -o nobackup "$1" "$2" || return
-	date +%s > "$pad_mnt"/02-ATIME
+	__sp_update_atime "$pad_mnt"
 }
 
 # 1: target
@@ -43,8 +46,8 @@ function __sp_unmount_pad() {
 }
 
 # 1: pad file
-function __sp_() {
-	tar -xOf "$1" "00-DESCRIPTION" 2> /dev/null
+function __sp_read_description() {
+	tar -xOf "$1" "00-DESCRIPTION" #2> /dev/null
 	return 0
 }
 
@@ -66,10 +69,10 @@ function __sp_pad_enter() {
 	)
 }
 
-function __sp_pad_list() {
+function __sp_pad_list_metadata() {
 	for f in "$SCRATCHPAD_STORAGE_DIR"/*.pad.gz; do
 		local desc
-		desc=$(__sp_get_description "$f" | head -n 1)
+		desc=$(__sp_read_description "$f" | head -n 1)
 		echo "$f;$desc"
 	done
 }
@@ -86,25 +89,65 @@ function __sp_hook_clear() {
 	test -f "$np" && head -n 5 "$np"
 }
 
+__sp_pad_choose() {
+	local cnt=0
+	local choices=()
+
+	while IFS=';' read -r FILE DESC _; do
+		echo "$cnt | $(basename "$FILE") | $DESC" >&2
+		choices+=( "$FILE" )
+		cnt=$((cnt+1))
+		
+	done <<< "$(__sp_pad_list_metadata | tail -n 10 | tac)"
+
+	read -rp "?> " CHOICE _
+
+	if [ -z "$CHOICE" ]; then
+		CHOICE=0
+	fi
+	
+	echo "${choices[$CHOICE]}"
+}
+
+# NOTE.
+# user facing commands
+
+function spls() {
+	while IFS=';' read -r FILE DESC _; do
+		echo "$(basename "$FILE") | $DESC"
+	done <<< "$(__sp_pad_list_metadata)"
+}
+
+function spenter() {
+	local pad_file pad_mnt
+	pad_file=$(__sp_pad_choose)
+	pad_mnt=$(__sp_genereate_mount_path "$(basename "$pad_file")" )
+
+	__sp_mount_pad "$pad_file" "$pad_mnt"
+	__sp_pad_enter "$pad_mnt"
+	__sp_unmount_pad "$pad_mnt"
+}
+
+function sp() {
+	# shellcheck disable=2031
+	test -n "$PADHOME" && return 1
+
+	local pad_file pad_id pad_mnt
+	pad_id=$(__sp_generate_id)
+	pad_file=$(__sp_genereate_file_path "$pad_id")
+	pad_mnt=$(__sp_genereate_mount_path "$(basename "$pad_file")" )
+
+	__sp_new_pad "$pad_file" "$SCRATCHPAD_TEMPLATE"
+
+	__sp_mount_pad "$pad_file" "$pad_mnt"
+	__sp_pad_enter "$pad_mnt"
+	__sp_unmount_pad "$pad_mnt"
+}
+
 # NOTE we only want to be injected on prompt when we are in a pad
 # shellcheck disable=2031
 if [ -n "$PADHOME" ]; then
 	HOOK_PROMPT+=(__sp_hook_prompt)
 	HOOK_CLEAR+=(__sp_hook_clear)
+	HOOK_ENTRY+=(__sp_hook_clear)
 fi
-
-function sp() {
-	local pad_file pad_id pad_mnt
-	pad_id=$(__sp_generate_id)
-	pad_file=$(__sp_genereate_file_path "$pad_id")
-	pad_mnt=$(__sp_genereate_mount_path "$pad_id")
-
-	__sp_new_pad "$pad_file" "$SCRATCHPAD_TEMPLATE"
-
-	__sp_mount_pad "$pad_file" "$pad_mnt"
-
-	__sp_pad_enter "$pad_mnt"
-
-	__sp_unmount_pad "$pad_mnt"
-	echo sp closed
-}
